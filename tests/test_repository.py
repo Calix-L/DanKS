5297e964ce283d29a0d9b2819b049b4d6aad4ffa
lin <linzhenxin@linzhenxindeMacBook-Air.local>
2026-08-26 13:28:31 +0800

    test: update README logo width expectation

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -79,7 +79,7 @@ def test_readme_visual_assets_are_versioned() -> None:
         assert "Kingsoft AI Product Center" in readme
         assert 'href="https://www.kingsoft.com/"' in readme
         assert 'src="assets/kingsoft-logo.png" alt=' in readme
-        assert 'width="320"' in readme
+        assert 'width="720"' in readme
 
     assert (
         '<h1 align="center">DanKS: State-of-the-art GuanDan AI</h1>'
